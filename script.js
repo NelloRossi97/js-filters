f30d1ad4b3fe93b065b0aa460bc1595f0a5c20fe
NelloRossi <rossianiello97@gmail.com>
2023-07-31 12:48:20 +0200

    added option to change image

--- script.js
+++ script.js
@@ -1,11 +1,7 @@
-
-
-
-
 document.addEventListener("DOMContentLoaded", function () {
-    let filters = document.querySelectorAll('.title');
     const imageInput = document.getElementById('imageInput');
     const previewImage = document.getElementById('previewImage');
+    const changeImage = document.getElementById('changeImage');
 
     imageInput.addEventListener('change', function(event) {
         const file = event.target.files[0];
@@ -17,11 +13,20 @@ document.addEventListener("DOMContentLoaded", function () {
                 previewImage.src = reader.result;
                 previewImage.style.display = 'block';
                 imageInput.style.display = 'none';
+                changeImage.style.display = 'block';
+            });
+
+            changeImage.addEventListener('click', function () {
+                previewImage.style.display = 'none';
+                imageInput.style.display = 'block';
+                changeImage.style.display = 'none';
             });
 
             reader.readAsDataURL(file);
         }
     });
+    
+    const filters = document.querySelectorAll('.title');
 });
 
 function showRange(filters) {
